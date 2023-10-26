19656d0f9030c926bf62c1fed0c40bfc10e0d144
dian <dianjeruto@gmail.com>
2023-10-26 14:50:45 +0300

    Added script.js

--- .vscode/script.js
+++
@@ -1,4 +0,0 @@
-let result = document.getElementById("result");
-let searchButton = document.getElementById("search-button")
-let url ="www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
-
---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const images = document.querySelector('.images')
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+.then(response => response.json())
+.then((data) => {
+    console.log(data)
+
+    const results = data.categories
+    for(const item of results){
+        const imagesDiv = document.createElement('div')
+        imagesDiv.innerHTML = `
+            <img src='${item.strCategoryThumb}'>
+        `
+images.appendChild(imagesDiv)
+    }
+})
+})
+
+
