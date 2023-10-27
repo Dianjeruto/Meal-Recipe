51fe9c6e51c38d5e3e4f71d7195d04086ec005f6
dian <dianjeruto@gmail.com>
2023-10-27 09:04:30 +0300

    added script.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Meal-Recipe
-It's a simple application .
+It's a simple application that guides one on how to prepare different types of meals in an organized way.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const images = document.querySelector('.images')
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+fetch("www.themealdb.com/api/json/v1/1/categories.php?f=a")
 .then(response => response.json())
 .then((data) => {
     console.log(data)
@@ -19,15 +19,21 @@ images.appendChild(imagesDiv)
 const ingredients = [];
 console.log(item.strCategoryThumb);
 console.log(item.strCategory)
-console.log()
 
 const ingredient = document.getElementById(ingredient);
-const item = document.createElement("ul");
+const images = document.createElement("ul");
 const recipe = document.getElementById(recipe)
 const showrecipe =document.getElementById
 showrecipe.addEventListener("click",
 
 )
+
+ingredient = images[1];
+measure = images['strmeasure' + count];
+count += 1;
+ingredients.push($)
+
+
 
 
 
