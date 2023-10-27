08ea338bb12ed461efa7540b2aaac12505d3e485
dian <dianjeruto@gmail.com>
2023-10-27 22:30:39 +0300

    Added script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const images = document.querySelector('.images');
     const searchBtn = document.getElementById("search-btn");
+
     fetch("https//themealdb.com/api/json/v1/1/categories.php?f=a")
     .then(response => response.json())
     .then((data) => {
         console.log(item);
-    
-    const count=1;
-    const ingredients=[];
-    const ingredient="";
-    const measure ="";
-    if (i.startsWith("strIngredient") && myMeal[i]);
-
-
-const userInput =document.getElementById(user-inp)
+        const userInput =document.getElementById(user-inp)
         const results = data.categories;
         for (const item of results) {
             const imagesDiv = document.createElement('div');
@@ -24,6 +17,20 @@ const userInput =document.getElementById(user-inp)
         }
     });
 });
+
+    const count=1;
+    const ingredients=[];
+    const ingredient="";
+    const measure ="";
+    if (i.startsWith("strIngredient") && myMeal[i]);
+ingredient=item[i];
+ingredients.push('${measure} ${ingredients}'
+);
+console.log(ingredients);
+result.innerHTML='<img src=${item.strCategoryThumb}>';
+
+
+
 
 
 
