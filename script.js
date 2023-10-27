5a9c375a20f79e46ea609f31808d58274440a816
dian <dianjeruto@gmail.com>
2023-10-27 20:29:47 +0300

    added index.html

--- script.js
+++ script.js
@@ -1,41 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const images = document.querySelector('.images')
-fetch("www.themealdb.com/api/json/v1/1/categories.php?f=a")
-.then(response => response.json())
-.then((data) => {
-    console.log(data)
-
-    const results = data.categories
-    for(const item of results){
-        const imagesDiv = document.createElement('div')
-        imagesDiv.innerHTML = `
-            <img src='${item.strCategoryThumb}'>
-        `
-images.appendChild(imagesDiv)
-    }
-})
-})
-const ingredients = [];
-console.log(item.strCategoryThumb);
-console.log(item.strCategory)
-
-const ingredient = document.getElementById(ingredient);
-const images = document.createElement("ul");
-const recipe = document.getElementById(recipe)
-const showrecipe =document.getElementById
-showrecipe.addEventListener("click",
-
-)
-
-ingredient = images[1];
-measure = images['strmeasure' + count];
-count += 1;
-ingredients.push($)
-
-
-
-
+    const images = document.querySelector('.images');
+    const searchBtn = document.getElementById("search-btn");
+    fetch("https//themealdb.com/api/json/v1/1/categories.php?f=a")
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+const userInput =document.getElementById(user-inp)
+        const results = data.categories;
+        for (const item of results) {
+            const imagesDiv = document.createElement('div');
+            imagesDiv.innerHTML = `
+                <img src='${item.strCategoryThumb}'>
+            `;
+            images.appendChild(imagesDiv);
+        }
+    });
+});
 
 
 
